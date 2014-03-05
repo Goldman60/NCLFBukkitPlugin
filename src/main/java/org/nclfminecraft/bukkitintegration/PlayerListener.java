80b7523cfcc8b93fd0128036bfa63fd69cd8108a
AJ Fite <goldman60@nclf.net>
2014-03-05 11:54:18 -0800

    Event skeletons and debug logging

--- src/main/java/org/nclfminecraft/bukkitintegration/PlayerListener.java
+++ src/main/java/org/nclfminecraft/bukkitintegration/PlayerListener.java
@@ -1,7 +1,9 @@
 package org.nclfminecraft.bukkitintegration;
 
+import static org.bukkit.Bukkit.getLogger;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 /**
@@ -13,20 +15,26 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onLogin(PlayerLoginEvent event) {
         /*
-            Add account to account list
-        */
-        
-        /* Check if player is registered
-                if not registered send message and check group membership,
-                Make necessary changes to group membership if required
-                
-                if registered check group membership
-                Make necessary changes to group membership if required
-        
-                Log result
+         Add account to account list
          */
         
-        event.getPlayer().getServer().broadcastMessage("Player login fired");
+        getLogger().info("NCLF onLogin fired.");
+
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        /* Check if player is registered
+         if not registered send message and check group membership,
+         Make necessary changes to group membership if required
+                
+         if registered check group membership
+         Make necessary changes to group membership if required
         
+         Log result
+         */
+        
+        getLogger().info("NCLF onJoin fired.");
+
     }
 }
